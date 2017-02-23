eb890a75a0120179890ca1cb6f4267ae2a4dac34
Woramet Muangsiri <krw127@hotmail.com>
2017-02-23 22:20:51 +0900

    Implement FeedViewModel

--- CattyImages/FeedViewModel.swift
+++ CattyImages/FeedViewModel.swift
@@ -10,6 +10,26 @@ import Foundation
 import RxSwift
 import Moya
 
-class FeedViewModel: NSObject {
+struct FeedViewModel {
+    var searchText = Variable<String>("")
+    var imageList: Observable<[CTImage]> {
+        return searchText
+            .asObservable()
+            .observeOn(MainScheduler.instance)
+            .flatMapLatest { text -> Observable<CTImageList> in
+                return self.getImages(txt: text)
+            }.flatMapLatest { li -> Observable<[CTImage]> in
+                return self.getCTImages(imglist: li)
+            }
+            
+    }
+    
+    func getImages(txt: String) -> Observable<CTImageList> {
+        return GettyProvider.request(.images(phrase: txt)).mapObject(CTImageList.self)
+    }
+    
+    func getCTImages(imglist: CTImageList) -> Observable<[CTImage]> {
+        return Observable.from(imglist.images)
+    }
     
 }
